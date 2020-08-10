56ba5cb36d7202c0f2c615d3444fd9696a53cf8b
cristobalaldezabal <cristobal.aldezabal@gmail.com>
2020-08-10 19:54:30 -0300

    wef

--- dos.js
+++ dos.js
@@ -7,5 +7,58 @@ b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
 */function mostrar()
 {
-  alert("dos");
+ 
+let marca;
+let precio;
+let peso;
+let tipo;
+let liquidocaro;
+let marcaliquidomaximo;
+let solidominimo;
+let marcasolidominimo;
+let acumuladorpeso = 0;
+let flag = 0;
+let resp = "s";
+
+
+  do {
+
+    marca=prompt("ingrese una marca");
+    precio=parseInt(prompt("ingrese un precio"));
+    peso=parseInt(prompt("ingrese un precio en kg"));
+    acumuladorpeso = acumuladorpeso + peso;
+
+    do {
+     
+  tipo = prompt("ingrese tipo solido o liquido");
+
+      switch (tipo) {
+        case "liquido":
+          if (flag == 0 || liquidocaro < precio) 
+          {
+            liquidocaro = precio;
+            marcaliquidomaximo = marca;
+            flag = 1;
+          }
+          break;
+        case "solido":
+          if(flag == 0 || solidominimo > precio)
+          {
+            solidominimo = precio;
+            marcasolidominimo = marca;
+            flag = 1;
+          }
+      }
+
+
+    } while (tipo != "liquido" && tipo != "solido");
+
+
+    resp = prompt("desea ingresar otro producto? s/n");
+  } while (resp == "s");
+
+ alert("el peso total de la compra es" +acumuladorpeso); 
+ alert("la marca del liquido mas caro es " +marcaliquidomaximo);
+ alert("la marca del solido mas barato es" + marcasolidominimo);
+
 }
--- uno.js
+++ uno.js
@@ -9,5 +9,53 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let peso;
+	let sexo;
+	let edad;
+
+	let contmujeres = 0; 
+	let acumuladoredad = 0;
+	let promedio;
+	let maxpeso;
+	let flag = 0;
+	let nombremaxpeso;
+
+	for (let i = 0; i < 5; i++) 
+	{
+
+
+	nombre = prompt("ingrese nombre");
+	peso = parseInt(prompt("ingrese peso"));
+
+
+	do {
+			sexo = prompt("ingreso sexo: f/m");
+
+			if (sexo == "f") {
+				contmujeres++;
+			}
+			else {
+				if (flag == 0 || maxpeso < peso) {
+					maxpeso = peso;
+					nombremaxpeso = nom;
+					flag = 1;
+				}
+			}
+
+		} while (sexo != "f" && sexo != "m");
+
+		edad = parseInt(prompt("ingrese edad"));
+		acumuladoredad = acumuladoredad + edad;
+		promedio = acumuladoredad / 5;
+
+
+	}
+
+
+	document.write("la cantidad de mujeres es de" + contmujeres);
+	document.write("el promedio total de la edad es" + promedio);
+	document.write("el hombre que mas pesa es " + nombremaxpeso + "y su peso es de " + maxpeso);
+
+
 }
